1373e15c48a94905c18d4c6777cc2bca5c460743
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-11-09 12:22:40 -0500

    finished constructBayesNet

--- inference.py
+++ inference.py
@@ -63,11 +63,13 @@ def constructBayesNet(gameState: hunters.GameState):
     "*** YOUR CODE HERE ***"
     variables = [PAC, GHOST0, GHOST1, OBS0, OBS1]
     edges = [(PAC, OBS0), (GHOST0, OBS0), (PAC, OBS1), (GHOST1, OBS1)]
-    variableDomainsDict[PAC] = [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)]
-    variableDomainsDict[GHOST0] = [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)]
-    variableDomainsDict[GHOST1] = [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)]
-
-    raiseNotDefined()
+    variableDomainsDict = {
+        PAC: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)],
+        GHOST0: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)],
+        GHOST1: [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)],
+        OBS0: [d for d in range(X_RANGE + Y_RANGE + MAX_NOISE - 1)],
+        OBS1: [d for d in range(X_RANGE + Y_RANGE + MAX_NOISE - 1)]
+    }
     "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
