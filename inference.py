f56ea48cc2aea14ab8f37a9f5dadd39ec0a9cfff
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-11-28 21:43:26 -0500

    q5b passed

--- inference.py
+++ inference.py
@@ -445,7 +445,15 @@ class InferenceModule:
         Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        if noisyDistance == None:
+            if ghostPosition == jailPosition:
+                return 1
+            else:
+                return 0
+        elif ghostPosition == jailPosition:
+            return 0
+        else:
+            return busters.getObservationProbability(noisyDistance, manhattanDistance(pacmanPosition, ghostPosition))
         "*** END YOUR CODE HERE ***"
 
     def setGhostPosition(self, gameState, ghostPosition, index):
